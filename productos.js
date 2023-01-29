3d0e4070190fd91e450140eecdff3d9fbbf260e3
franielkenny <francogrimoldidelgado@hotmail.com>
2023-01-29 15:26:57 -0300

    Tarea finalizada

--- productos.js
+++ productos.js
@@ -1,12 +1,12 @@
 class ProductManager {
-    #productos
+    productos
 
     constructor () {
-       this.#productos = []
+       this.productos = []
     }
 
     getProducts () {
-        return this.#productos;
+        return this.productos;
     }
 
     addProduct (title, description, price, thumbnail, code, stock){
@@ -18,18 +18,53 @@ class ProductManager {
             code,
             stock,
         }
-        if (this.#productos.length === 0) {
-            producto.id = 1;
+        if (this.productos.length === 0) {
+          producto.id = 1;
         } else {
-            producto.id = this.#productos[this.#productos.length -1].id +1;
+          producto.id = this.productos.length + 1;
         }
-        this.#productos.push(evento);
+        if 
+          (this.productos.find ((e) => e.code === code)) {
+            return console.error ("El código está repetido!");
+          }
+          this.productos.push(producto);
+        }
+        
+  
+    getProductById(productoId) {
+        const buscarProducto = this.productos.find(
+          (producto) => producto.id === productoId
+        );
+        if (!buscarProducto) {
+          console.error("Producto no encontrado");
+        } else {
+          console.log(buscarProducto);
+        }
+      }
+
     }
 
+ //pruebas
 
+const pm = new ProductManager ();
+pm.addProduct (
+  "Producto X",
+  "Descripición X",
+  100,
+  "Imagen X",
+  "X1",
+  10,
+);
 
-}
+pm.addProduct(
+  "Producto Y",
+  "Descripción Y",
+  200,
+  "Imagen Y",
+  "Y1",
+  20,
+);
 
-
-
-
+console.log(pm.getProducts());
+pm.getProductById(1);
+pm.getProductById(2);
